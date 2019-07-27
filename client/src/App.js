df856c776003bdcc28e4f9106018f511adcd4d11
nursaid <uznet@yahoo.com>
2019-07-27 01:44:24 -0700

    added components

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Books from "./pages/Books";
+import Saved from "./pages/Saved";
+import NoMatch from "./pages/NoMatch";
+import Nav from "./components/Nav";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Nav />
+        <Switch>
+          <Route path ="/" exact component={Books}></Route>
+          <Route path ="/books" exact component={Saved}></Route>
+          <Route path ="/books/:id" component={Saved}></Route>
+          <Route component ={NoMatch}></Route>
+        </Switch>
+      </div>
+    </Router>
+
   );
 }
 
---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+
+const Footer = () => (
+	<footer className="page-footer">
+		<div className="container-fluid text-center">
+			<p id = "footer">Nursaid | <a href="https://github.com/nursaids/google-book-search">GitHub </a>
+			</p>
+		</div>
+	</footer>
+);
+
+export default Footer;
